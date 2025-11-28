cd00d32d41cccfd3b7e0bcf2c5beacc34b899d33
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-28 13:25:37 +0000

    Remove map zooming

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,15 @@
 var map = L.map('map', {
     center: [42, 11],
     zoom: 2.57,
-    zoomSnap: 0
+    zoomSnap: 0,
+    dragging: false,
+    boxZoom: false,
+    zoomControl: false,
+    doubleClickZoom: false
+
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
