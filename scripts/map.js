b475777c13cda6b0e1fc0a6196fae457cbf509a3
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-28 18:46:34 +0000

    Add first info box

--- scripts/map.js
+++ scripts/map.js
@@ -19,7 +19,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var circle = L.circle([51.508, -0.11], {
+var circle = L.circle([14, 51], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
@@ -27,6 +27,14 @@ var circle = L.circle([51.508, -0.11], {
 }).addTo(map);
 
 
-circle.on("click", (e) => { 
-});
 
+let latlng = circle.getLatLng();
+let point = map.latLngToContainerPoint(latlng);
+
+const el = document.createElement("div");
+el.id = "my-html";
+el.style.position = "absolute";
+el.style.left = point.x + "px";
+el.style.top = point.y + "px";
+el.style.zIndex = 9999;
+document.getElementById("map").appendChild(el);
