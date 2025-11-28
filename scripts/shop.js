0545da01e6cc16542ee81b782eccf86bf19efb55
irevapa <irene.vapalahti@outlook.com>
2025-11-28 14:41:21 +0200

    stores

---
+++ scripts/shop.js
@@ -0,0 +1,11 @@
+const storeItems = [
+    {id: 1, name: "Voucher", price: 100, type: "voucher"},
+    {id: 2, name: "Postcard from Africa", price: 200, type: "postcard"},
+    {id: 3, name: "Postcard from Asia", price: 200, type: "postcard"},
+    {id: 4, name: "Postcard from Australia", price: 200, type: "postcard"},
+    {id: 5, name: "Postcard from Europe", price: 200, type: "postcard"},
+    {id: 6, name: "Postcard from North America", price: 200, type: "postcard"},
+    {id: 7, name: "Postcard from South America", price: 200, type: "postcard"},
+];
+
+
