1c03ef032b832ceabc5ac085128860f2f56e4a15
Miko Gronroos <miko.gronroos@gmail.com>
2025-12-02 21:36:46 +0000

    Add functionality to get all airports from the backend

--- scripts/map.js
+++ scripts/map.js
@@ -19,22 +19,40 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var circle = L.circle([14, 51], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 275000
-}).addTo(map);
+async function getAirports(){
+  try {
+    const response = await fetch('http://127.0.0.1:3000/getNewAirports');  
+    const airports = await response.json();         
+    return airports;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getAirports().then(
+  function(value) {showAirports(value);}
+);
+
+function showAirports(items) {
+  for (var i = 0; i < items.length; i++) {
+    var circle = L.circle([items[i][1], items[i][2]], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 275000
+    }).addTo(map);
+
+    let latlng = circle.getLatLng();
+    let point = map.latLngToContainerPoint(latlng);
+  }
+/*
+  const el = document.createElement("div");
+  el.id = "my-html";
+  el.style.position = "absolute";
+  el.style.left = point.x + "px";
+  el.style.top = point.y + "px";
+  el.style.zIndex = 9999;
+  document.getElementById("map").appendChild(el);*/
+}
 
 
-
-let latlng = circle.getLatLng();
-let point = map.latLngToContainerPoint(latlng);
-
-const el = document.createElement("div");
-el.id = "my-html";
-el.style.position = "absolute";
-el.style.left = point.x + "px";
-el.style.top = point.y + "px";
-el.style.zIndex = 9999;
-document.getElementById("map").appendChild(el);
