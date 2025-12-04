ca981ee0f07f165f419c44bd6305e8833c60c0ff
Sofia Ravi <fiialina@outlook.com>
2025-12-04 14:33:44 +0200

    added login.html and login.js

---
+++ scripts/login.js
@@ -0,0 +1,59 @@
+
+document.getElementById("loginbutton").onclick = async () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (!username) {
+    document.getElementById("message").textContent = "Please enter a username.";
+    return;
+  }
+
+  if (!password) {
+    document.getElementById("message").textContent = "Please enter a password.";
+    return;
+  }
+
+  const data = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ username, password })
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:5000/login", data);
+    const json = await response.json();
+    document.getElementById("message").textContent = json.message;
+  } catch (e) {
+    document.getElementById("message").textContent = "Could not connect to the server";
+  }
+};
+
+
+document.getElementById("registerbutton").onclick = async () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (!username) {
+    document.getElementById("message").textContent = "Please enter a username.";
+    return;
+  }
+
+  if (!password) {
+    document.getElementById("message").textContent = "Please enter a password.";
+    return;
+  }
+
+  const data = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ username, password })
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:5000/register", data);
+    const json = await response.json();
+    document.getElementById("message").textContent = json.message;
+  } catch (e) {
+    document.getElementById("message").textContent = "Could not connect to the server";
+  }
+};
